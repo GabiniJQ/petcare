07b6b417af8bbae54e531e50bb44e7fe44455a7c
Jose Quintana <joseguardoq@gmail.com>
2026-01-23 18:36:46 -0500

    Initial Commit

---
+++ src/pages/home/Hero.tsx
@@ -0,0 +1,27 @@
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        
+        <div>
+          <img src='' alt=''/>
+        </div>
+
+        menu
+      </div>
+
+      <div>
+        <h1></h1>
+
+        <h2></h2>
+
+        <div>
+          find
+          cuidar
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/pages/home/MenuMobile.tsx
@@ -0,0 +1,28 @@
+
+const MenuMobile = () => {
+  return (
+    <div className='flex flex-col gap-12'>
+      <a href='#home' className='text-center'>
+        INICIO
+      </a>
+
+      <a href='#home' className='text-center'>
+        SERVICIOS
+      </a>
+
+      <a href='#home' className='text-center'>
+        TESTIMONIOS
+      </a>
+
+      <a href='#home' className='text-center'>
+        BENEFICIOS
+      </a>
+
+      <a href='#home' className='text-center'>
+        CONTACTO
+      </a>
+    </div>
+  )
+}
+
+export default MenuMobile
